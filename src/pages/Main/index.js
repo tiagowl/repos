d4314a887d852032896192c0111c35aaa96fad0a
= <=>
2020-10-21 18:04:59 -0300

    enviando repositório para a nuvem

---
+++ src/pages/Main/index.js
@@ -0,0 +1,115 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+export default function Main(){
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [alert, setAlert] = useState(null);
+
+    useEffect(()=>{
+        const repoStorage = localStorage.getItem('repos');
+
+        if(repoStorage){
+            setRepositorios(JSON.parse(repoStorage));
+        }
+    }, []);
+
+    useEffect(()=>{
+        localStorage.setItem('repos', JSON.stringify(repositorios));
+    }, [repositorios]);
+
+    const handleSubmit = useCallback((e)=>{
+
+        e.preventDefault();
+
+        async function submit(){
+            
+            setLoading(true);
+            setAlert(false);
+
+            try{
+
+                if(newRepo === ''){
+                    throw new Error('Voce precisa indicar um repositório');
+                }
+
+                const response = await api.get(`repos/${newRepo}`);
+
+                const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+                if(hasRepo){
+                    throw new Error('Repositorio duplicado');
+                }
+
+                const data = {
+                    name: response.data.full_name
+                }
+
+                setRepositorios([...repositorios, data]);
+                setNewRepo('');
+            }catch(error){
+                console.log(error);
+                setAlert(true);
+            }finally{
+                setLoading(false);
+            }
+            
+        }
+
+        submit();
+    }, [newRepo, repositorios]);
+
+    function handleinputChange(e){
+        setNewRepo(e.target.value);
+        setAlert(null);
+    }
+
+    const handleDelete = useCallback((repo)=>{
+        const find = repositorios.filter(r=>r.name !== repo);
+        setRepositorios(find);
+    }, [repositorios])
+
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25}/>
+                Meus repositórios
+            </h1>
+
+            <Form onSubmit={handleSubmit} error={alert} >
+                <input type="text" value={newRepo} onChange={handleinputChange} />
+                <SubmitButton Loading={loading ? 1 : 0} >
+                    {loading ? (
+                        <FaSpinner color="#FFF" size={14} />
+                    ) : (
+                        <FaPlus color="#FFF" size={14} />
+                    )
+
+                    }
+                    
+                </SubmitButton>
+            </Form>
+
+            <List>
+                    {repositorios.map(repo => (
+                       <li key={repo.name} >
+                           <span>
+                           <DeleteButton onClick={()=>handleDelete(repo.name)} >
+                                <FaTrash size={14} />
+                           </DeleteButton>
+                           {repo.name}
+                           </span>
+                           <Link to={`/repositorio/${encodeURIComponent(repo.name)}`} >
+                                <FaBars size={20} />
+                           </Link>
+                       </li> 
+                    ))}
+            </List>
+        </Container>
+    );
+}
---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root{
+        min-height: 100%;
+    }
+
+    body{
+        background: white;
+        font-size: 14px;
+        webkit-font-smoothing: antialiased !important;
+    }
+
+    body, button, input{
+        color: #222;
+        font-size: 14px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    button{
+        cursor: pointer;
+
+    }
+`;
